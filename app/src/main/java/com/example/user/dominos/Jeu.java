bf0cebb9e535b5a84d97615709bb86d191b70ce0
Axel Vicard <vicard@polytech.unice.fr>
2014-10-15 11:51:59 +0200

    Ajout des grid view et grid layout.

--- app/src/main/java/com/example/user/dominos/Jeu.java
+++ app/src/main/java/com/example/user/dominos/Jeu.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 
 public class Jeu extends Activity {
@@ -12,6 +16,15 @@ public class Jeu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jeu);
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(Jeu.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 
